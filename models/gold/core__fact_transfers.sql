454b48ed7152cafc8404d81a6781b6b781168a52
sam <sam@flipsidecrypto.com>
2022-10-13 20:19:59 +0800

    gold view

---
+++ models/gold/core__fact_transfers.sql
@@ -0,0 +1,18 @@
+{{ config(
+    materialized = 'view'
+) }}
+
+SELECT 
+    block_id, 
+    block_timestamp, 
+    blockchain, 
+    chain_id, 
+    tx_id, 
+    tx_succeeded, 
+    transfer_type, 
+    sender, 
+    amount, 
+    currency, 
+    receiver
+
+FROM {{ ref('silver__transfers') }}
