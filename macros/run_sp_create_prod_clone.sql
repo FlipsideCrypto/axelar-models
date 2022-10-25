ce01f8095ff8e7cdfcb7afe91effd8c287956bf8
eric-laurello <102970824+eric-laurello@users.noreply.github.com>
2022-10-25 15:56:27 -0400

    Update run_sp_create_prod_clone.sql

--- macros/run_sp_create_prod_clone.sql
+++ macros/run_sp_create_prod_clone.sql
@@ -1,6 +1,6 @@
 {% macro run_sp_create_prod_clone() %}
 {% set clone_query %}
-call osmosis._internal.create_prod_clone('axelar', 'axelar_dev', 'internal_dev');
+call axelar._internal.create_prod_clone('axelar', 'axelar_dev', 'internal_dev');
 {% endset %}
 
 {% do run_query(clone_query) %}
