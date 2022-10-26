5f001bb3566f378f1bc9971af5242365694476d4
Julius Remigio <14811322+juls858@users.noreply.github.com>
2022-10-26 16:26:20 -0700

    - add alert pass through script - update dbt_run.yml to use new alert.py

---
+++ alert.py
@@ -0,0 +1,29 @@
+import sys
+
+import click
+import pkg_resources
+
+from cli_passthrough import cli_passthrough
+from cli_passthrough.utils import write_to_log
+
+version = pkg_resources.get_distribution("cli-passthrough").version
+CONTEXT_SETTINGS = {"ignore_unknown_options": True, "allow_extra_args": True}
+
+
+# CLI entry point
+@click.command(context_settings=CONTEXT_SETTINGS)
+@click.pass_context
+def cli(ctx):
+    """Entry point"""
+    write_to_log("\nNEW CMD = {}".format(" ".join(sys.argv[1:])))
+    write_to_log("\nNEW CMD = {}".format(" ".join(sys.argv[1:])), "stderr")
+
+    exit_status = cli_passthrough(" ".join(ctx.args), interactive=False)
+
+    # TODO - call code to parse dbt results and send slack alerts here.
+
+    sys.exit(exit_status)
+
+
+if __name__ == "__main__":
+    cli(obj={})
