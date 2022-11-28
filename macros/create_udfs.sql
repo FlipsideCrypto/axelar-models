5c47e89ec581affae0dd8fb7afed2d4030363e72
yulike <xiuyangguan@gmail.com>
2022-11-28 18:02:15 -0500

    updated the function

--- macros/create_udfs.sql
+++ macros/create_udfs.sql
@@ -2,6 +2,9 @@
     {% set sql %}
     CREATE schema if NOT EXISTS silver;
 {{ create_js_hex_to_int() }};
+{{ create_udtf_get_base_table(
+        schema = "streamline"
+    ) }}
 
     {% endset %}
     {% do run_query(sql) %}
