d9d073ce16e08411131983d1600a0a6450fa4164
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-05-09 09:55:17 -0400

    cross dbs refs

---
+++ models/silver/crossdb/silver__address_labels.sql
@@ -0,0 +1,33 @@
+{{ config(
+    materialized = 'incremental',
+    unique_key = "address",
+    incremental_strategy = 'merge',
+    cluster_by = 'system_created_at::DATE'
+) }}
+
+SELECT
+    system_created_at,
+    insert_date,
+    blockchain,
+    address,
+    creator,
+    label_type,
+    label_subtype,
+    address_name,
+    project_name
+FROM
+    {{ source(
+        'crosschain',
+        'address_labels'
+    ) }}
+WHERE
+    blockchain = 'axelar'
+
+{% if is_incremental() %}
+AND system_created_at :: DATE >= (
+    SELECT
+        MAX(system_created_at) :: DATE -2
+    FROM
+        {{ this }}
+)
+{% endif %}
