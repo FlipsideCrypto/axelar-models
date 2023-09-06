74162cf1ff75e94b8eb0e8974c37879726f2ec66
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-09-06 12:57:23 -0400

    adjust schedule

--- models/bronze/bronze__transactions.sql
+++ models/bronze/bronze__transactions.sql
@@ -4,41 +4,9 @@
     unique_key = 'file_name',
     cluster_by = ['_inserted_timestamp::date']
 ) }}
+-- depends_on: {{ ref('bronze__streamline_transactions') }}
+-- depends_on: {{ ref('bronze__streamline_FR_transactions') }}
 
-WITH meta AS (
-
-    SELECT
-        registered_on,
-        last_modified,
-        GREATEST(
-            last_modified,
-            registered_on
-        ) AS _inserted_timestamp,
-        file_name
-    FROM
-        TABLE(
-            information_schema.external_table_files(
-                table_name => '{{ source( "bronze", "txs_details") }}'
-            )
-        ) A
-
-{% if is_incremental() %}
-WHERE
-    GREATEST(
-        registered_on,
-        last_modified
-    ) >= DATEADD(
-        DAY,
-        -2,(
-            SELECT
-                COALESCE(MAX(_INSERTED_TIMESTAMP), '1970-01-01' :: DATE) max_INSERTED_TIMESTAMP
-            FROM
-                {{ this }})
-        )
-    )
-{% else %}
-)
-{% endif %}
 SELECT
     VALUE,
     _partition_by_block_id,
@@ -49,18 +17,28 @@ SELECT
     ) :: INT AS block_id_requested,
     metadata,
     DATA,
-    tx_hash :: STRING AS tx_id,
-    tx_result,
+    DATA :hash :: STRING AS tx_id,
+    DATA :tx_result AS tx_result,
     file_name,
     TO_TIMESTAMP(
         m._inserted_timestamp
     ) AS _inserted_timestamp
 FROM
-    {{ source(
-        'bronze',
-        'txs_details'
-    ) }}
-    JOIN meta m
-    ON m.file_name = metadata$filename
+
+{% if is_incremental() %}
+{{ ref('bronze__streamline_transactions') }}
+{% else %}
+    {{ ref('bronze__streamline_FR_transactions') }}
+{% endif %}
+
+m
+
+{% if is_incremental() %}
 WHERE
-    DATA: error IS NULL
+    _inserted_timestamp >= (
+        SELECT
+            MAX(_inserted_timestamp)
+        FROM
+            {{ this }}
+    )
+{% endif %}
