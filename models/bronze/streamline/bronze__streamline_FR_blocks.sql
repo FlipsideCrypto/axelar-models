11c5110eb74cdfe0e2b66541e020271da8494d0a
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-09-13 17:09:59 -0400

    update view logic streamline

--- models/bronze/streamline/bronze__streamline_FR_blocks.sql
+++ models/bronze/streamline/bronze__streamline_FR_blocks.sql
@@ -3,7 +3,7 @@
 ) }}
 {{ streamline_external_table_FR_query(
     "blocks",
-    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 4), '_', 1) AS INTEGER )",
+    partition_function = "CAST(SPLIT_PART(SPLIT_PART(file_name, '/', 3), '_', 1) AS INTEGER )",
     partition_name = "_partition_by_block_id",
     unique_key = "block_number"
 ) }}
