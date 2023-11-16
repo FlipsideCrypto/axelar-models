309a9b41d94f11a0d32532440c09ab405aa6a47d
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-11-16 15:15:38 -0500

    merge main

--- models/gold/core/core__fact_blocks.sql
+++ models/gold/core/core__fact_blocks.sql
@@ -2,12 +2,18 @@
     materialized = 'view'
 ) }}
 
-SELECT 
-    block_id, 
-    block_timestamp, 
-    chain_id, 
-    tx_count, 
-    proposer_address, 
-    validator_hash
-FROM 
+SELECT
+    block_id,
+    block_timestamp,
+    chain_id,
+    tx_count,
+    proposer_address,
+    validator_hash,
+    COALESCE (
+        blocks_id,
+        {{ dbt_utils.generate_surrogate_key(
+            ['chain_id','block_id']
+        ) }}
+    ) AS fact_blocks_id
+FROM
     {{ ref('silver__blocks') }}
