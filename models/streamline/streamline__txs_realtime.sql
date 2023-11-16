fda70190f6e2e9ac4666f35f5265a82241f236e7
Eric Laurello <eric.laurello@flipsidecrypto.com>
2023-11-16 15:12:52 -0500

    streamline speed up

--- models/streamline/streamline__txs_realtime.sql
+++ models/streamline/streamline__txs_realtime.sql
@@ -6,6 +6,13 @@
     )
 ) }}
 
+WITH last_3_days AS (
+
+    SELECT
+        block_number
+    FROM
+        {{ ref("_block_lookback") }}
+)
 SELECT
     A.block_number,
     A.tx_count
@@ -18,5 +25,11 @@ WHERE
     A.tx_count IS NOT NULL
     AND A.tx_count > 0
     AND b.block_number IS NULL
+    AND A.block_number >= (
+        SELECT
+            block_number
+        FROM
+            last_3_days
+    )
 ORDER BY
     1 ASC
