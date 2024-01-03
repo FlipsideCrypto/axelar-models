c456266be6b3e86dfdc700bb9cab1804e00cf120
mattromano <mattromano88@gmail.com>
2024-01-03 05:29:53 -0800

    add pagination to validator metadata api pull

--- models/bronze/bronze_api/bronze_api__get_validator_metadata.sql
+++ models/bronze/bronze_api/bronze_api__get_validator_metadata.sql
@@ -9,7 +9,24 @@ WITH call AS (
             'https://api.axelarscan.io',{},{'path':'/cosmos/staking/v1beta1/validators','module':'lcd'}
         ) AS resp,
         SYSDATE() AS _inserted_timestamp
+),
+call_2 AS (
+    SELECT
+        ethereum.streamline.udf_api(
+            'POST',
+            'https://api.axelarscan.io',{},{'path':'/cosmos/staking/v1beta1/validators?pagination.key=FMrVj6JowYsKRr2zLFCWofPkvPYV','module':'lcd'}
+        ) AS resp,
+        SYSDATE() AS _inserted_timestamp
 )
+SELECT
+    i.value :operator_address :: STRING AS address,
+    i.value AS data,
+    _inserted_timestamp
+FROM call_2, 
+LATERAL FLATTEN(
+    input => resp :data :validators
+) i
+UNION ALL
 SELECT
     i.value :operator_address :: STRING AS address,
     i.value AS data,
