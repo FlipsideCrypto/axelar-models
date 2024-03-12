242ef0f3338fed0bdce49a433ae186f218f1aa4e
Julius Remigio <14811322+juls858@users.noreply.github.com>
2024-03-12 14:18:38 -0700

    Update streamline__transactions_realtime.sql and package-lock.yml

--- models/streamline/realtime/streamline__transactions_realtime.sql
+++ models/streamline/realtime/streamline__transactions_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'txs_v2', 'exploded_key', '[\"result\", \"txs\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','5000')}}))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'txs_v2', 'exploded_key', '[\"result\", \"txs\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
@@ -25,7 +25,7 @@ ORDER BY
 )
 SELECT
     block_number AS partition_key,
-    {{ target.database }}.live.udf_api(
+    live.udf_api(
         'POST',
         '{service}/{Authentication}',
         OBJECT_CONSTRUCT(
@@ -56,5 +56,3 @@ FROM
     blocks
 ORDER BY
     block_number
-LIMIT
-    1000
