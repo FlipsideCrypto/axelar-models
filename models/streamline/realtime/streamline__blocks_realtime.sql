0e891ce31364e8be313f9f042b4c8d6f7f76dd10
Eric Laurello <eric.laurello@flipsidecrypto.com>
2024-04-16 12:54:09 -0400

    batch size

--- models/streamline/realtime/streamline__blocks_realtime.sql
+++ models/streamline/realtime/streamline__blocks_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'blocks_v2', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','50000')}}))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'blocks_v2', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','500')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
--- models/streamline/realtime/streamline__transactions_realtime.sql
+++ models/streamline/realtime/streamline__transactions_realtime.sql
@@ -1,7 +1,7 @@
 {{ config (
     materialized = "view",
     post_hook = if_data_call_function(
-        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'txs_v2', 'exploded_key', '[\"result\", \"txs\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','1000')}}))",
+        func = "{{this.schema}}.udf_rest_api(object_construct('sql_source', '{{this.identifier}}', 'external_table', 'txs_v2', 'exploded_key', '[\"result\", \"txs\"]', 'sql_limit', {{var('sql_limit','100000')}}, 'producer_batch_size', {{var('producer_batch_size','100000')}}, 'worker_batch_size', {{var('worker_batch_size','500')}}))",
         target = "{{this.schema}}.{{this.identifier}}"
     )
 ) }}
