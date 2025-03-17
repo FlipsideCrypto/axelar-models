2782861d8fd4c896a4195ad15c00a98f6944817f
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-03-17 16:35:33 -0400

    rpc cutover

--- models/streamline/realtime/streamline__tx_counts_realtime.sql
+++ models/streamline/realtime/streamline__tx_counts_realtime.sql
@@ -27,7 +27,7 @@ SELECT
     ) AS partition_key,
     live.udf_api(
         'POST',
-        '{Service}/rpc',
+        '{Service}/{Authentication}',
         OBJECT_CONSTRUCT(
             'Content-Type',
             'application/json'
@@ -49,7 +49,7 @@ SELECT
                 FALSE
             )
         ),
-        'vault/prod/axelar/blockjoy/mainnet'
+        'vault/prod/axelar/drpc/mainnet'
     ) AS request
 FROM
     blocks
