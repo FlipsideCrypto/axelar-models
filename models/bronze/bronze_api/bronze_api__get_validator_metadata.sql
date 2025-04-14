c94cca351f39b13d0754ca2bf613aeaa82c95264
Eric Laurello <eric.laurello@flipsidecrypto.com>
2025-04-14 15:45:57 -0400

    hotfix for validator metadata

--- models/bronze/bronze_api/bronze_api__get_validator_metadata.sql
+++ models/bronze/bronze_api/bronze_api__get_validator_metadata.sql
@@ -1,37 +1,40 @@
 {{ config(
-  materialized = 'table'
+    materialized = 'table'
 ) }}
 
 WITH call AS (
+
     SELECT
         live.udf_api(
-            'POST',
-            'https://api.axelarscan.io',{},{'path':'/cosmos/staking/v1beta1/validators','module':'lcd'}
+            'GET',
+            'https://api.axelarscan.io/validator/getValidators',{},{}
         ) AS resp,
         SYSDATE() AS _inserted_timestamp
-),
+) {# ,
 call_2 AS (
     SELECT
         live.udf_api(
             'POST',
-            'https://api.axelarscan.io',{},{'path':'/cosmos/staking/v1beta1/validators?pagination.key=FMrVj6JowYsKRr2zLFCWofPkvPYV','module':'lcd'}
+            'https://api.axelarscan.io',{},{ 'path' :'/cosmos/staking/v1beta1/validators?pagination.key=FMrVj6JowYsKRr2zLFCWofPkvPYV',
+            'module' :'lcd' }
         ) AS resp,
         SYSDATE() AS _inserted_timestamp
-)
+) #}
 SELECT
     i.value :operator_address :: STRING AS address,
-    i.value AS data,
+    i.value AS DATA,
     _inserted_timestamp
-FROM call_2, 
-LATERAL FLATTEN(
-    input => resp :data :validators
-) i
-UNION ALL
+FROM
+    call,
+    LATERAL FLATTEN(
+        input => resp :data :data
+    ) i {# UNION ALL
 SELECT
     i.value :operator_address :: STRING AS address,
-    i.value AS data,
+    i.value AS DATA,
     _inserted_timestamp
-FROM call, 
-LATERAL FLATTEN(
-    input => resp :data :validators
-) i
+FROM
+    call,
+    LATERAL FLATTEN(
+        input => resp :data :validators
+    ) i #}
